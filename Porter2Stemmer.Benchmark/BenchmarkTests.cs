67af58a3bb92426ac612cb3b46f9ee75a1bda8af
shavakerzh <a.shestakov4895@gmail.com>
2022-05-27 23:37:41 +0300

    add benchmark project

---
+++ Porter2Stemmer.Benchmark/BenchmarkTests.cs
@@ -0,0 +1,19 @@
+﻿using BenchmarkDotNet.Attributes;
+using Porter2Stemmer;
+
+
+namespace Benchmark
+{
+    public class BenchmarkTests
+    {
+        private readonly EnglishPorter2Stemmer _stemmer;
+
+        public BenchmarkTests()
+        {
+            _stemmer = new EnglishPorter2Stemmer();
+        }
+
+        [Benchmark]
+        public StemmedWord AbandonmentTest() => _stemmer.Stem("abandonment");
+    }
+}
